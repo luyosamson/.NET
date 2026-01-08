e8ed844b946dfd1d48caf623c2f7e47541cb759b
luyosamson <luyosamson@gmail.com>
2026-01-08 18:26:21 +1200

    Control structures

---
+++ ifElse/ControlStructures/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ControlStructures;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter number to determine its status");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if (num > 0) 
+        {
+            Console.WriteLine($"{num} is a positive number");
+        }
+        else if(num < 0)
+
+        {
+            Console.WriteLine($"{num} is a Negative number");
+        }
+        else
+        {
+        
+            Console.WriteLine($"{num} is  Zero ");
+        }
+    }
+    
+  
+}
