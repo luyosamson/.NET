7c1687a1fa95425dfc9590b5b531235abe7be6d1
luyosamson <luyosamson@gmail.com>
2026-01-09 22:56:16 +1200

    Control structures

---
+++ ifElse/oddeven/oddeven/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace oddeven;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number");
+        int num = int.Parse(Console.ReadLine());
+        int result = num % 2;
+        if (result == 0)
+        {
+            Console.WriteLine($"{num} is an Even number");
+        }
+        else
+        {
+            Console.WriteLine($"{num} is an Odd number");
+        }
+    }
+}
