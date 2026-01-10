7515be06b21034d2654b31cad7a6653edce5f0d7
luyosamson <luyosamson@gmail.com>
2026-01-10 23:28:44 +1200

    Loops in C#

---
+++ ifElse/Loops/Loops/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Loops;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       Console.WriteLine("Print number from 1-10");
+       int numx = 1;
+       while (numx <= 10)
+       {
+         Console.WriteLine($"{numx}");
+         numx++;
+       }
+    }
+}
